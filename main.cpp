7b4986b394a4b3a1ea2895baa9b33a79d6d0dfc2
kfbaclagon <201910322@feualabang.edu.ph>
2022-06-04 01:36:40 +0800

    trial 1

--- main.cpp
+++ main.cpp
@@ -1,15 +1,92 @@
-#include <iostream>
+#include<iostream>
+#include<cstring>
+#include<stdlib.h>
+#include<ctype.h>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main() {
-	
-	
-	cout << "Programming Languages";
-	cout << "push";
-	
-	return 0;
-	
-}
+string arr[] = { "INT", "CHAR", "BLN", "FLT", "DBL", "VOID", "STR", "main",
+				  "IN", "OUT", "return", "std", "iostream", "endl" };
 
-//testing ulit gamit vs
-//test push 
+bool isKeyword (string a)
+	{
+		for (int i = 0; i < 14; i++)
+	{
+	if (arr[i] == a) {
+	
+			return true;
+		}
+	}
+	return false;
+	}
+
+
+int main ()
+{
+
+	std::ifstream file("prog.txt");
+	std::string x;
+	string code="";
+
+	while (std::getline(file, x)) {
+		code+=x;
+	}
+
+	string s="";
+		for (int i = 0; i < code.size (); i++)
+	{
+
+		if (code[i] != ' ')
+		s += code[i];
+
+	else {
+		if (s == "+" || s == "-" || s == "*" || s == "/" || s == "^" || s == "&&" || s == "||" ||
+			s == "=" || s == "==" || s == "&" || s == "|" || s == "%" || s == "++" || s == "--" || 
+			s == "+=" || s == "-=" || s == "/=" || s == "*=" || s == "%=")
+	{
+		cout << s <<" is an operator" << endl;
+		s = "";
+	}
+
+	else if (isKeyword (s)) {
+		cout << s <<" is a keyword" << endl;
+		s = "";
+	}
+	
+	else if (s == "(" || s == "{" || s == "[" || s == ")" || s == "}" || s == "]" || s == "<" ||
+			 s == ">" || s == "()" || s == ";" || s == "<<" || s == ">>" || s == "," || s == "#" || s == "~" || s == "@")
+	{
+		cout << s <<" is a symbol" << endl;
+		s = "";
+	}
+	
+	else if (s == "\n" || s == "" || s == "")
+	{
+		cout << endl;
+		s = "";
+	}
+	
+	else if (isdigit (s[0]))
+	{
+	
+		int x = 0;
+			if (!isdigit (s[x++]))
+				{
+					continue;
+				} 
+		else
+		{
+			cout << s <<" is a number" << endl;
+			s = "";
+		}
+	}
+	
+	else {
+		cout << s <<" is an identifier" << endl;
+		s = "";
+			}
+
+		}
+	}
+}
