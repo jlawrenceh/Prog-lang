487617ad9666b980cb29a7c78dce9d6a12983722
walfredcutaran <cutaranwalfred1218@gmail.com>
2022-07-02 22:22:26 +0800

    lexical errors v0.2
    *identifier w/ symbols error
    *identifier w/ numeric chars at the start
    *value w/ symbols error

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include<array>
 #include<string>
 #include<vector>
+#include <algorithm>
 using namespace std;
 
 
@@ -66,58 +67,45 @@ bool isSymbol (string a){
 bool islexicalerror(string a){
     if (a.find_first_not_of(legal_chars) != string::npos){
     	return true;
-	} else {
+	}
+	 else {
 		return false;
 	}
 } 
 
-	// identifier cannot start with a number (conflict)
-bool islexicalerror_2 (string a){
-	for(int i = 0; i < nums.length(); i++){
-		if(a.at(0) == nums[i]){
-			return true;
-		} else {
-			return false;
-		}
-	}
+//identifer cannot start with number(s)
+bool invalid_identifier(string a){
+	
+	if (!((a[0] >= 'a' && a[0] <= 'z') || 
+		  (a[0] >= 'A' && a[0] <= 'Z') || 
+		   a[0] == '_')){
+          	return true;		
+		} 
+		else 
+		{
+		 return false;
+		}    
+}
+
+bool is_number(string a){
+	string::const_iterator it = a.begin();
+    while (it != a.end() && isdigit(*it)) ++it;
+    return !a.empty() && it == a.end();
 }
 
 void view_lexical_errors(){
 	for (i = lexical_errors.begin(); i != lexical_errors.end(); ++i){		
-		if(i == lexical_errors.end() - 1){
-			cout << *i <<"\n";
-		} 
+		cout << *i <<"\n";
 	}
 }
+
+
     
 
 void parser(string str){
 		string s="";
 		int len = str.length();
-		bool read_str_lit = false;
 		
-	for (int i = 0; i < len; i++)
-	{
-
-		if (read_str_lit == true)
-		{
-			s+= str[i];
-			if (str[i] == '\"')
-			{
-				read_str_lit = false;
-				lex.push_back(s);
-				cout << s <<" is a string literal";
-				s = "";
-			}
-			continue;
-		}	
-		
-		if (str[i] == '\"')
-		{
-			read_str_lit = true;
-			s+= str[i];
-			continue;
-		}
 	for (int i = 0; i < len; i++)
 	{
 
@@ -128,53 +116,60 @@ void parser(string str){
 		if (isOperator(s))
 	{
 		lex.push_back(s);
-		cout << s <<" is an operator" << endl;
+		cout << s <<" is an operator\n";
 		s = "";
 	}
 
 	else if (isKeyword (s)) {
 		
 		lex.push_back(s);
-		cout << s <<" is a keyword" << endl;
+		cout << s <<" is a keyword\n";
 		s = "";
 	}
 	
 	else if (isSymbol(s))
 	{
 		lex.push_back(s);
-		cout << s <<" is a symbol" << endl;
+		cout << s <<" is a symbol\n";
 		s = "";
 	}
 	
 	else if (s == "\n" || s == "" || s == "")
 	{
-		cout << endl;
+		cout << "\n";
 		s = "";
 	}
 	
-	else if (isdigit (s[0])) //conflict aayusin pa
+	else if (isdigit (s[0])) 
 	{
 		int x = 0;
+		string s_copy = s;
+		s_copy.erase(remove(s_copy.begin(), s_copy.end(), '.'), s_copy.end()); // to remove '.' from floating point numbers temporarily
 			if (!isdigit (s[x++]))
-				{
-					continue;
-				} 
-		else
-		{
-			lex.push_back(s);
-			cout << s <<" is a number" << endl;
-			s = "";
-		}
+			{
+				continue;
+			}
+			else if(invalid_identifier(s) && !(is_number(s_copy))){ //if identifier has numbers at the start
+				lexical_errors.push_back(s);
+				cout << s << " is a lexical error\n";
+				s = "";
+			}
+
+			else
+			{
+				lex.push_back(s);
+				cout << s <<" is a number\n";
+				s = "";
+			}
 	}
 	
 	else {
 		lex.push_back(s);
-		
-		if(islexicalerror(s) || islexicalerror_2(s)){
+		if(islexicalerror(s)){
 			lexical_errors.push_back(s);
-			cout << s << " is an lexical error\n";
+			cout << s << " is an lexical error\n"; //if identifier has symbol(s)
 		} else {
-			cout << s <<" is an identifier" << endl;
+			cout << s <<" is an identifier\n";
 		}
 		s = "";
 			}
@@ -182,11 +177,13 @@ void parser(string str){
 		}
 	}
 	
+	int cnt = 0;
 	cout<<endl;
 	for (i = lex.begin(); i != lex.end(); ++i){
-		
+		cnt++;
 		if(i == lex.end() - 1){
 			cout << *i;
+			
 		} else {
 			cout << *i << ", ";
 		}
@@ -244,8 +241,12 @@ int main(){
             break;
         }
         case 3:{ // lexical errors
-            cout << "Lexical errors...\n";
-            view_lexical_errors();
+        	if(lexical_errors.empty()){
+				cout<<"No errors found...\n";
+		    } else {
+		    	cout << "Lexical errors...\n";
+            	view_lexical_errors();
+			}
             break;
         }
         case 4:{ // syntax errors
@@ -279,4 +280,5 @@ int main(){
 	
     return 0;
 }
+
 
