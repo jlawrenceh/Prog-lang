f1060283f4fdb435d4945ae545f0f1bbcc8857b0
Lawrence Hernandez <jlawrencehernandez@gmail.com>
2022-07-04 12:23:44 +0800

    division by 0 not allowed

--- PROJECT/main.cpp
+++ PROJECT/main.cpp
@@ -95,6 +95,7 @@ int main(){
         case 5:{ // semantic errors
             cout << "View Semantic errors"<<endl;
             assignment();
+            division();
             break;
         }
         case 6:{ // exit
